322bf80e7eb5ea9f60740ee6bb62b45289134e52
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2023-12-15 15:26:41 +0100

    Stage 1: Print page size

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,17 @@
 use anyhow::{bail, Result};
-use std::fs::File;
-use std::io::prelude::*;
+use std::{env, fs::File, io::Read};
 
 fn main() -> Result<()> {
     // Parse arguments
-    let args = std::env::args().collect::<Vec<_>>();
-    match args.len() {
-        0 | 1 => bail!("Missing <database path> and <command>"),
-        2 => bail!("Missing <command>"),
-        _ => {}
-    }
+    let db_path = env::args()
+        .nth(1)
+        .expect("Missing <database path> and <command>");
+    let command = env::args().nth(2).expect("Missing <command>");
 
     // Parse command and act accordingly
-    let command = &args[2];
     match command.as_str() {
         ".dbinfo" => {
-            let mut file = File::open(&args[1])?;
+            let mut file = File::open(&db_path)?;
             let mut header = [0; 100];
             file.read_exact(&mut header)?;
 
@@ -23,11 +19,8 @@ fn main() -> Result<()> {
             #[allow(unused_variables)]
             let page_size = u16::from_be_bytes([header[16], header[17]]);
 
-            // You can use print statements as follows for debugging, they'll be visible when running tests.
-            println!("Logs from your program will appear here!");
-
             // Uncomment this block to pass the first stage
-            // println!("database page size: {}", page_size);
+            println!("database page size: {page_size}");
         }
         _ => bail!("Missing or invalid command passed: {}", command),
     }
