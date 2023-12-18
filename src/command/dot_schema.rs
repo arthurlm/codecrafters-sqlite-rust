a019436c4bf8ba7da16108c0a9b352dba8956e9a
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2023-12-18 15:21:16 +0100

    Do not filter anything in .schema command

--- src/command/dot_schema.rs
+++ src/command/dot_schema.rs
@@ -2,8 +2,6 @@ use crate::database::Database;
 
 pub fn exec(db: &mut Database) {
     for schema_row in db.schema_rows().expect("Fail to read DB schema") {
-        if schema_row.schema_type == "table" && !schema_row.name.starts_with("sqlite_") {
-            println!("{}", schema_row.sql);
-        }
+        println!("{}", schema_row.sql);
     }
 }
